8289700c8e99941ac9e808b52d4ebcd9b96471c7
Steve Lianoglou <slianoglou@gmail.com>
2012-02-25 20:58:50 -0500

    cooking

--- R/spectrum.R
+++ R/spectrum.R
@@ -1,3 +1,11 @@
+spectrumFeatures <- function(strings, degree=4) {
+  features <- lapply(degree, function(deg) {
+    xx <- oligonucleotideFrequency(strings, 4)
+    xx[colSums(xx) > 0]
+  })
+  do.call(cbind, features)
+}
+
 ## Manual spectrum kernel
 if (FALSE) {
 
